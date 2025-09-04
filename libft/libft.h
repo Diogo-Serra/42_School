0b0eef98d468003b88be2013074558e09582445d
Di Code <di-code@outlook.com>
2025-09-05 00:02:58 +0100

    added new exercises

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 18:05:58 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/05 00:00:19 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/05 00:02:47 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,10 @@ size_t	ft_strlen(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
 
-#endif
+#endif /* LIBFT_H */
 
 /* Tests
+
 int	main(int argc, char **argv)
 {
 	int	result;
