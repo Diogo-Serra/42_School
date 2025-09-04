27d40755ac691f365dd8b7d27867ca5dbd14ea8f
Di Code <di-code@outlook.com>
2025-09-04 23:34:51 +0100

    added new exercises

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 18:05:58 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/04 22:01:44 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/04 22:46:31 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdio.h>
 
 size_t	ft_strlen(const char *string);
+char	*ft_strcpy(char *dest, const char *source);
 
 #endif
 
@@ -29,6 +30,19 @@ int	main(int argc, char **argv)
 	{
 		result = ft_strlen(argv[1]);
 		printf("strlen: %d\n", result);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*ptr;
+	char	dest[20];
+
+	if (argc == 2)
+	{
+		ptr = ft_strcpy(dest, argv[1]);
+		printf("strcpy: %s\n", ptr);
 	}
 	return (0);
 }*/
