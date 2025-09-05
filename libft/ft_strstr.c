51aee6e0adf388e31bf13fcd2bd708468c92ea77
Di Code <di-code@outlook.com>
2025-09-05 17:49:14 +0100

    added new exercises

--- libft/ft_strstr.c
+++ libft/ft_strstr.c
@@ -6,13 +6,13 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 17:05:19 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/05 17:40:46 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/05 17:49:06 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strstr(const char *haystack, const char *needle)
+char	*ft_strstr(const char *haystack, const char *needle)
 {
 	size_t	i;
 	size_t	j;
@@ -23,11 +23,9 @@ char	*strstr(const char *haystack, const char *needle)
 	while (haystack[i])
 	{
 		j = 0;
-		while (haystack[i + j] && needle[j]
-			&& ft_tolower((unsigned char)haystack[i + j])
-			== ft_tolower((unsigned char)needle[j]))
+		while ((haystack[i + j] && needle[j]) && (haystack[i + j] == needle[j]))
 			j++;
-		if (needle[j] == '\0')
+		if (!needle[j])
 			return ((char *)&haystack[i]);
 		i++;
 	}
