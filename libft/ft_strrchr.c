075ca69668fda6466eca64cccae0439c03b04521
Di Code <di-code@outlook.com>
2025-09-05 15:45:59 +0100

    added new exercises

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -1,14 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtchr.c                                       :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 15:25:40 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/05 15:25:48 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/05 15:45:38 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char	*ft_strrchr(const char *s, int c)
+{
+	const char	ch = (char)c;
+	size_t		i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	if (s[i] == ch)
+		return (char *)&s[i];
+	while (i > 0)
+	{
+		i--;
+		if (s[i] == ch)
+			return (char *)&s[i];
+	}
+	return (NULL);
+}
