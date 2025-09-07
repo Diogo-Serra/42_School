773e4ab95812079ac18c7682499d33abf2c6aa5e
Di Code <di-code@outlook.com>
2025-09-07 18:49:04 +0100

    update libft project

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 18:37:26 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/07 18:40:05 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/07 18:48:58 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
+	const unsigned char	*p;
+	unsigned char		uc;
+	size_t				i;
 
+	p = (const unsigned char *)s;
+	uc = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (p[i] == uc)
+			return ((void *)&p[i]);
+		i++;
+	}
+	return (NULL);
 }
