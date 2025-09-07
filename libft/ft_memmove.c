8613c1293faf4ead2adb00a160cbe5671d958ae8
Di Code <di-code@outlook.com>
2025-09-07 18:35:22 +0100

    update libft project

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 17:46:06 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/07 18:26:17 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/07 18:27:35 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char		*d = (unsigned char *)dest;
-	const unsigned char	*s = (const unsigned char *)src;
+	unsigned char		*d;
+	const unsigned char	*s;
 
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
 	if (dest == src || n == 0)
 		return (dest);
 	if (d < s)
