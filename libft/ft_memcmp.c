230ae2629a2201f8c4880c872f5eadc861e1e202
Di Code <di-code@outlook.com>
2025-09-07 18:57:27 +0100

    update libft project

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,8 +6,26 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 18:49:59 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/07 18:50:08 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/07 18:57:15 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*p1;
+	const unsigned char	*p2;
+	size_t				i;
+
+	p1 = (unsigned char *) s1;
+	p2 = (unsigned char *) s2;
+	i = 0;
+	while (i < n)
+	{
+		if (p1[i] != p2[i])
+			return ((int)(p1[i] - p2[i]));
+		i++;
+	}
+	return (0);
+}
