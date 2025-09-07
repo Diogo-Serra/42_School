0032d79a13325d135784627b584afefad2d7738c
Di Code <di-code@outlook.com>
2025-09-07 16:54:18 +0100

    added new exercises

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 16:50:29 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/07 16:51:15 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/07 16:54:10 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	bzero(void *s, size_t n)
 {
+	unsigned char 	*p;
+	size_t			i;
 
+	p = (unsigned char *) s;
+	i = 0;
+	while (i < n)
+	{
+		p[i] = '\0';
+		i++;
+	}
+	
 }
