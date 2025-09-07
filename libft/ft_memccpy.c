830e1c56e1b36963c1abfed9fefee4b9f4d764d2
Di Code <di-code@outlook.com>
2025-09-07 17:41:26 +0100

    added new exercises

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 17:17:14 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/07 17:22:10 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/07 17:40:28 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
+	unsigned char 	*d;
+	unsigned char 	*s;
+	unsigned char 	ch;
+	size_t			i;
 
+	d = (unsigned char *) dest;
+	s = (unsigned char *) src;
+	ch = (unsigned char) c;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		if (s[i] == ch)
+			return ((void *)&d[i + 1]);
+		i++;
+	}
+	return (NULL);
 }
