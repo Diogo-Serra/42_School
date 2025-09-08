9a2abc5e354c49af182559f75ce910ed4f901d8f
Di Code <di-code@outlook.com>
2025-09-08 15:36:35 +0100

    update libft project

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 15:35:13 by diserra           #+#    #+#             */
+/*   Updated: 2025/09/08 15:36:26 by diserra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t			len;
+	unsigned int	i;
+	char			*out;
+
+	if (!s || !f)
+		return (NULL);
+	len = 0;
+	while (s[len])
+		len++;
+	out = (char *)malloc(len + 1);
+	if (!out)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		out[i] = f(i, s[i]);
+		i++;
+	}
+	out[len] = '\0';
+	return (out);
+}
