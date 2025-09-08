2002b5a10503267c9540597ab0d3ebc4be08c47c
Di Code <di-code@outlook.com>
2025-09-08 16:39:54 +0100

    update libft project

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,13 +6,21 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 16:34:39 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/08 16:35:00 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/08 16:39:42 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <unistd.h>
 
 void	ft_putstr_fd(char const *s, int fd)
 {
+	size_t	i;
 
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+		i++;
+	(void) write(fd, s, i);
 }
