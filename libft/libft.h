4623b60a0e66ea50f6f94f129c9a7aeaca943c08
Di Code <di-code@outlook.com>
2025-09-08 20:00:29 +0100

    update libft project

--- libft/libft.h
+++ libft/libft.h
@@ -6,14 +6,13 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 18:05:58 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/08 19:29:25 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/08 20:00:13 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <string.h>
 # include <stdlib.h>
 # include <stddef.h>
 
