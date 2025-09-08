3ebb2278f163d22cad2763acd30ba337bd4c1193
Di Code <di-code@outlook.com>
2025-09-08 15:48:44 +0100

    update libft project

---
+++ libft/ft_strsub.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 15:47:49 by diserra           #+#    #+#             */
+/*   Updated: 2025/09/08 15:48:26 by diserra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*out;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	out = (char *)malloc(len + 1);
+	if (!out)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		out[i] = s[start + i];
+		i++;
+	}
+	out[len] = '\0';
+	return (out);
+}
