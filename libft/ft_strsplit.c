676684b22248b5323d9cb3f1d41da29313fcb2f7
Di Code <di-code@outlook.com>
2025-09-08 18:17:07 +0100

    update libft project

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -1,40 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/08 18:05:39 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/08 18:16:58 by diserra          ###   ########.fr       */
+/*   Created: 2025/09/08 18:12:08 by diserra           #+#    #+#             */
+/*   Updated: 2025/09/08 18:12:27 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	is_sp(char c)
+char	**ft_strsplit(char const *s, char c)
 {
-	return (c == ' ' || c == '\n' || c == '\t');
-}
 
-char	*ft_strtrim(char const *s)
-{
-	size_t	i;
-	size_t	j;
-	char	*out;
-
-	if (!s)
-		return (NULL);
-	i = 0;
-	while (s[i] && is_sp(s[i]))
-		i++;
-	j = ft_strlen(s);
-	while (j > i && is_sp(s[j - 1]))
-		j--;
-	out = (char *)malloc(j - i + 1);
-	if (!out)
-		return (NULL);
-	ft_memcpy(out, s + i, j - i);
-	out[j - i] = '\0';
-	return (out);
 }
