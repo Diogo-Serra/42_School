516db7e1744263b69593d29a4faf1a62e52b65d6
Di Code <di-code@outlook.com>
2025-09-08 17:17:43 +0100

    update libft project

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 13:59:27 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/05 14:48:21 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/08 17:17:34 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t	dlen;
-	size_t	slen;
-	size_t	i;
+	size_t dlen;
+	size_t slen;
+	size_t i;
 
+	dlen = 0;
 	while (dlen < size && dest[dlen] != '\0')
 		dlen++;
+	slen = 0;
 	while (src[slen] != '\0')
 		slen++;
 	if (dlen == size)
 		return (size + slen);
-	while (src[i] != '\0' && i + 1 < (size - dlen))
+	i = 0;
+	while (src[i] && dlen + i + 1 < size)
 	{
 		dest[dlen + i] = src[i];
 		i++;
