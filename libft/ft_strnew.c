a47acf375f466dc8deeeafcc86c92fc26fdfa8f8
Di Code <di-code@outlook.com>
2025-09-08 14:51:10 +0100

    update libft project

--- libft/ft_strnew.c
+++ libft/ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 14:44:05 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/08 14:44:29 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/08 14:50:58 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 char	*ft_strnew(size_t size)
 {
+	char	*p;
+	size_t	i;
 
+	p = (char *)malloc(size + 1);
+	if (!p)
+		return (NULL);
+	i = 0;
+	while (i <= size)
+		p[i++] = '\0';
+	return (p);
 }
