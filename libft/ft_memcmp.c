220cde2367130ba1692997b12d3ef1e4e2e9c88d
Di Code <di-code@outlook.com>
2025-09-14 21:36:19 +0100

    updates

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 18:49:59 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/14 21:33:46 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/14 21:36:16 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,15 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	const unsigned char	*p1;
 	const unsigned char	*p2;
-	size_t				i;
 
 	p1 = (const unsigned char *)s1;
 	p2 = (const unsigned char *)s2;
-	i = 0;
-	while (i < n)
+	while (n--)
 	{
-		if (p1[i] != p2[i])
-			return ((int)(p1[i] - p2[i]));
-		i++;
+		if (*p1 != *p2)
+			return ((int)(*p1 - *p2));
+		p1++;
+		p2++;
 	}
 	return (0);
 }
