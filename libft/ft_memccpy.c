d043e6cf01dbf28143dd5ee865b2dd2e5c02f29c
Di Code <di-code@outlook.com>
2025-09-14 21:08:23 +0100

    updates

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,11 +6,32 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 17:17:14 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/12 18:55:28 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/14 21:08:20 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+	unsigned char	ch;
+	size_t			i;
+
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	ch = (unsigned char) c;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		if (s[i] == ch)
+			return ((void *)&d[i + 1]);
+		i++;
+	}
+	return (NULL);
+}
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
