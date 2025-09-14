e6d09ff123ba383aa7774495fc2e966288024b38
Di Code <di-code@outlook.com>
2025-09-14 21:33:17 +0100

    updates

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 18:37:26 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/14 21:18:39 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/14 21:33:15 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,9 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	const unsigned char	*p;
 	unsigned char		uc;
-	size_t				i;
 
 	p = (const unsigned char *)s;
 	uc = (unsigned char)c;
-	i = 0;
-	while (i < n)
-	{
-		if (p[i] == uc)
-			return ((void *)&p[i]);
-		i++;
-	}
-	return (NULL);
-}
-
-void	*ft_memchr(const void *s, int c, size_t n)
-{
-	const unsigned char	*p;
-	unsigned char		uc;
-
-	p = (const unsigned char *)s;
-	uc = (unsigned char)c;
-
 	while (n--)
 	{
 		if (*p == uc)
