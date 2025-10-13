8894ae0ef8246684ee1a5b97a25278b21765ccc0
Di Code <di-code@outlook.com>
2025-10-13 22:37:58 +0100

    updates

--- reloaded/ex12/ft_iterative_factorial.c
+++ reloaded/ex12/ft_iterative_factorial.c
@@ -6,26 +6,9 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 22:01:19 by diserra           #+#    #+#             */
-/*   Updated: 2025/10/13 22:37:01 by diserra          ###   ########.fr       */
+/*   Updated: 2025/10/13 22:37:49 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
-
-void	ft_putnbr(int n)
-{
-	char	c;
-
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		n = -n;
-	}
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	c = (n % 10) + '0';
-	write(1, &c, 1);
-}
 
 int	ft_iterative_factorial(int nb)
 {
