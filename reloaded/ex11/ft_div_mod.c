be50a4c0c191aa3c9c4db028bf177caa9a4a8393
Di Code <di-code@outlook.com>
2025-10-13 21:59:27 +0100

    updates

---
+++ reloaded/ex11/ft_div_mod.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 21:50:44 by diserra           #+#    #+#             */
+/*   Updated: 2025/10/13 21:59:19 by diserra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+/*
+void	ft_putnbr(int	n)
+{
+	char	c;
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = -n;
+	}
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	c = (n % 10) + '0';
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	int	div;
+	int	mod;
+	int	a = 10;
+	int	b = 3;
+
+	ft_div_mod(a, b, &div, &mod);
+	write(1, "DIV: ", 5);
+	ft_putnbr(div);
+	write(1, "\n", 1);
+	write(1, "MOD: ", 5);
+	ft_putnbr(mod);
+	write(1, "\n", 1);
+}*/
