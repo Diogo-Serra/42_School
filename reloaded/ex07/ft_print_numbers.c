990b7929a56e9521c87799eed5780584e8d9b531
Di Code <di-code@outlook.com>
2025-10-13 20:31:54 +0100

    updates

--- reloaded/ex07/ft_print_numbers.c
+++ reloaded/ex07/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 20:13:14 by diserra           #+#    #+#             */
-/*   Updated: 2025/10/13 20:31:14 by diserra          ###   ########.fr       */
+/*   Updated: 2025/10/13 20:31:46 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ void	ft_print_numbers(void)
 	}
 	write(1, "\n", 1);
 }
-
+/*
 int	main(void)
 {
 	ft_print_numbers();
-}
+}*/
