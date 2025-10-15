c21272e50d0bbe2d589bf1957254dccb75004512
Diogo Alexandre Soares Serra <diosoare@c2r9s1.42lisboa.com>
2025-10-15 11:16:06 +0100

    updates

--- reloaded/ex20/ft_strdup.c
+++ reloaded/ex20/ft_strdup.c
@@ -6,32 +6,32 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 19:20:53 by diosoare          #+#    #+#             */
-/*   Updated: 2025/10/14 23:54:20 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/10/14 21:01:14 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include <unistd.h>
 
-size_t	ft_strlen(const char *src)
+int	ft_strlen(char *src)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
 	while (src[i])
-		i++;
+	{
+		write(1, &src[i], 1);
+	}
 	return (i);
 }
 
-char	*ft_strdup(char *src)
+char *ft_strdup(char *src)
 {
-	char		*dest;
-	size_t		i;
-	size_t		len;
+    char	*dest;
+	int		i;
+	int		len;
 
-	if (src == NULL)
-		return (NULL);
-	len = (ft_strlen(src));
-	dest = malloc((len + 1) * sizeof(*dest));
+	len = ft_strlen(src);
+	dest = (char *)malloc(len + 1);
 	if (!dest)
 		return (NULL);
 	i = 0;
