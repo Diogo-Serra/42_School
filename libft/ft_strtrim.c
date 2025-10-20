4e8c7d287463386e0815646b556bf2c7b490eaf9
Di Code <di-code@outlook.com>
2025-10-20 18:06:24 +0100

    updates

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,32 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 17:56:42 by diosoare          #+#    #+#             */
-/*   Updated: 2025/10/20 17:56:43 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/10/20 18:06:13 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	len;
+	char	*out;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	len = end - start;
+	out = (char *)malloc(len + 1);
+	if (!out)
+		return (NULL);
+	ft_memcpy(out, s1 + start, len);
+	out[len] = '\0';
+	return (out);
+}
