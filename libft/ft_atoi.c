80c9d5d0b6d4910c3a34dbecf2976a24ec10b73f
Diogo Alexandre Soares Serra <diosoare@c2r3s7.42lisboa.com>
2025-10-23 19:50:55 +0100

    updates

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,27 +6,11 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 08:24:43 by diosoare          #+#    #+#             */
-/*   Updated: 2025/10/23 19:49:14 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/10/23 19:50:35 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int	better_atoi(const char *s)
-{
-	int	res;
-	int	sgn;
-
-	sgn = 1;
-	res = 0;
-	while ((*s == ' ') || (*s >= 9 && *s <= 13))
-		s++;
-	if (*s == '+' || *s++ == '-')
-		sgn = (*(s - 1) == '+') - (*(s - 1) == '-');
-	while (*s >= '0' && *s <= '9')
-		res = (res * 10) + (*s++ - '0');
-	return (res * sgn);
-}
 
 int	ft_atoi(const char *s)
 {
