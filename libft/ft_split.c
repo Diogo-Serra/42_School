d6d09654bd5f966e88d53d1429d3cd43dcd35606
Diogo Alexandre Soares Serra <diosoare@c2r3s7.42lisboa.com>
2025-10-23 17:17:25 +0100

    updates

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,88 +6,96 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 17:37:43 by diosoare          #+#    #+#             */
-/*   Updated: 2025/10/20 17:47:11 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/10/23 17:17:12 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	word_len(char const *s, char c)
+size_t	ft_wordlen(const char *src, char c)
 {
 	size_t	i;
 
+	if (!src)
+		return (0);
 	i = 0;
-	while (s[i] && s[i] != c)
+	while (src[i] && src[i] != c)
 		i++;
 	return (i);
 }
 
-static int	count_words(char const *s, char c)
+int	ft_nwords(const char *src, char c)
 {
-	int	cnt;
-	int	in;
+	size_t	i;
+	int		words;
 
-	cnt = 0;
-	in = 0;
-	while (*s)
-	{
-		if (*s != c && !in)
-		{
-			in = 1;
-			cnt++;
-		}
-		else if (*s == c)
-			in = 0;
-		s++;
-	}
-	return (cnt);
-}
-
-static char	**free_all(char **tab, int i)
-{
-	while (i-- > 0)
-		free(tab[i]);
-	free(tab);
-	return (NULL);
-}
-
-static char	*dup_word(char const *s, char c)
-{
-	size_t	len;
-	char	*w;
-
-	len = word_len(s, c);
-	w = (char *)malloc(len + 1);
-	if (!w)
-		return (NULL);
-	ft_memcpy(w, s, len);
-	w[len] = '\0';
-	return (w);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**tab;
-	int		i;
-
-	if (!s)
-		return (NULL);
-	tab = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1));
-	if (!tab)
-		return (NULL);
 	i = 0;
-	while (*s)
+	words = 0;
+	if (!src)
+		return (0);
+	while (src[i])
 	{
-		while (*s && *s == c)
-			s++;
-		if (!*s)
-			break ;
-		tab[i] = dup_word(s, c);
-		if (!tab[i])
-			return (free_all(tab, i));
-		s += word_len(s, c);
+		if (src[i] != c && (i == 0 || src[i - 1] == c))
+            words++;
+        i++;
+	}
+	return (words);
+}
+
+char	*ft_strnchr(const char *s, int c)
+{
+	char		ch;
+	size_t		i;
+
+	i = 0;
+	ch = (const char)c;
+	while (s[i])
+	{
+		if (s[i] == ch && s[i + 1] != ch)
+			return ((char *)&s[i]);
 		i++;
 	}
-	tab[i] = NULL;
+	return ((char *)s);
+}
+
+char	**ft_split(const char *src, char c)
+{
+	char	**tab;
+	char	*next;
+	int		nwords;
+	int		i;
+
+	i = 0;
+	if (!src)
+		return (NULL);
+	nwords = ft_nwords(src, c);
+	tab = ft_calloc(nwords + 1, sizeof(char));
+	if (!tab)
+		return (NULL);
+	next = ft_strnchr(src, (int)c);
+	while (i <= nwords)
+	{
+		tab[i] = calloc((size_t)ft_wordlen + 1, sizeof(char));
+		ft_memcpy(tab[i], next, ft_wordlen(next, c));
+		nwords--;
+		i++;
+	}
 	return (tab);
+}
+
+int	main(void)
+{
+	char	**tab;
+	char	*src = "Hello World Escola 42";
+	char	sep = ' ';
+	int		nwords;
+	int		i;
+
+	nwords = ft_nwords(src, sep);
+	tab = ft_split(src, sep);
+	i = 0;
+	while (i <= nwords)
+		printf("%s\n", tab[i++]);
+	free(tab);
+	return (0);
 }
