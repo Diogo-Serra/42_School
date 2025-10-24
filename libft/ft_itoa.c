e151b7d78ab9d39da4c3fbd29639524864a1c8fd
Di Code <di-code@outlook.com>
2025-10-24 03:54:08 +0100

    updates

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,33 +6,52 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 08:28:32 by diosoare          #+#    #+#             */
-/*   Updated: 2025/10/23 19:48:37 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/10/24 03:53:35 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	get_len(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n != 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
 char	*ft_itoa(int n)
 {
-	char	*s;
-	char	num[11];
-	int		data[3];
+	char	*out;
+	int		len;
+	long	nb;
 
-	data[0] = 10;
-	data[1] = (n >= 0) - (n < 0);
-	while (n)
-	{
-		num[data[0]--] = (n % 10) + '0';
-		n /= 10;
-	}
-	if (data[1] < 0)
-		num[data[0]--] = '-';
-	data[2] = 10 - data[0];
-	s = ft_calloc(data[2] + 1, sizeof(char));
-	if (!s)
+	nb = n;
+	len = get_len(nb);
+	out = (char *)malloc(sizeof(char) * (len + 1));
+	if (!out)
 		return (NULL);
-	data[0] = 10;
-	while (data[2]--)
-		s[data[2]] = num[data[0]--];
-	return ((char *)s);
+	out[len] = '\0';
+	len--;
+	if (nb == 0)
+		out[0] = '0';
+	if (nb < 0)
+	{
+		out[0] = '-';
+		nb = -nb;
+	}
+	while (nb > 0)
+	{
+		out[len] = (nb % 10) + '0';
+		nb = nb / 10;
+		len--;
+	}
+	return (out);
 }
