56068f3255f547b12bd9a5fa7dd2d255e97d5bff
Diogo Alexandre Soares Serra <diosoare@c2r1s12.42lisboa.com>
2025-10-27 13:14:29 +0000

    updates

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 08:34:14 by diosoare          #+#    #+#             */
-/*   Updated: 2025/10/27 12:53:34 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/10/27 13:14:24 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,19 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	char	arr[12];
 	long	nb;
-	int		i;
+	size_t	i;
 
 	nb = n;
-	i = sizeof(arr);
+	i = sizeof(arr) - 1;
 	if (nb < 0)
 		nb = -nb;
 	if (nb == 0)
 		arr[--i] = '0';
 	while (nb > 0)
 	{
-		arr[--i] = (nb % 10) + '0';
-		nb /= 10; 
+		ft_putchar_fd((nb % 10) + '0', fd);
+		nb /= 10;
 	}
 	if (n < 0)
-		arr[--i] = '-';
-	ft_putstr_fd(arr + i, fd);
-}
-
-int	main(void)
-{
-	ft_putnbr_fd(25, 1);
-	return (0);
+		ft_putchar_fd('-', fd);
 }
