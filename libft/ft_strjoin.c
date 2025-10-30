b5489d8af796f00ef62c163bcae7f121a474632b
Di Code <di-code@outlook.com>
2025-10-30 01:04:55 +0000

    updates

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 08:39:45 by diosoare          #+#    #+#             */
-/*   Updated: 2025/10/26 01:16:50 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/10/30 01:04:35 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	l[2];
+	size_t	len1;
+	size_t	len2;
 	char	*out;
 
 	if (!s1 || !s2)
 		return (NULL);
-	l[0] = ft_strlen(s1);
-	l[1] = ft_strlen(s2);
-	out = (char *)ft_calloc(l[0] + l[1] + 1, sizeof(char));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	out = (char *)ft_calloc(len1 + len2 + 1, sizeof(char));
 	if (!out)
 		return (NULL);
-	ft_memcpy(out, s1, l[0]);
-	ft_memcpy(out + l[0], s2, l[1]);
+	ft_memcpy(out, s1, len1);
+	ft_memcpy(out + len1, s2, len2);
 	return (out);
 }
