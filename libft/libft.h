97287f13c8f92dd961c1f7a172601e5a301c9704
Di Code <di-code@outlook.com>
2025-10-30 19:41:36 +0000

    updates

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 13:09:16 by diosoare          #+#    #+#             */
-/*   Updated: 2025/10/30 17:39:03 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/10/30 19:41:29 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,29 +77,29 @@ int		ft_toupper(int c);
 /* =========================================================*/
 /* 6) Numeric conversion                                    */
 /* =========================================================*/
+char	*ft_itoa(int n);
 int		ft_atoi(const char *s);
 
 /* =========================================================*/
 /* 7) Raw memory primitives                                 */
 /* =========================================================*/
-void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
-void	ft_bzero(void *s, size_t n);
 
 /* =========================================================*/
 /* 9) String functional utilities                           */
 /* =========================================================*/
-char	*ft_itoa(int n);
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char	**ft_split(char const *s, char c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 /* =========================================================*/
 /* 10) I/O (file descriptor variants)                       */
