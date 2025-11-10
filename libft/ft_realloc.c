bc610aed77c56cf18ab9eab0fd9685d701f4cae1
Diogo Alexandre Soares Serra <diosoare@c2r9s1.42lisboa.com>
2025-11-10 10:46:15 +0000

    updates

--- libft/ft_realloc.c
+++ libft/ft_realloc.c
@@ -6,36 +6,37 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 09:58:12 by diosoare          #+#    #+#             */
-/*   Updated: 2025/11/10 10:17:51 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/11/10 10:46:08 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_realloc(void *ptr, size_t new_size)
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
-	size_t	old_size;
 	void	*new_pointer;
+	size_t	to_copy;
 
-	if (!ptr)
-		ptr = malloc(new_size);
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	old_size = *((size_t *)ptr - 1);
+	if (!ptr)
+		return (malloc(new_size));
 	new_pointer = malloc(new_size);
 	if (!new_pointer)
 		return (NULL);
 	if (old_size < new_size)
 	{
-		ft_memcpy(new_pointer, ptr, old_size);
+		to_copy = old_size;
 	}
 	else
 	{
-		ft_memcpy(new_pointer, ptr, new_size);
+		to_copy = new_size;
 	}
+	if (to_copy)
+		ft_memcpy(new_pointer, ptr, to_copy);
 	free(ptr);
 	return (new_pointer);
 }
