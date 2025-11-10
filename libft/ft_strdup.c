22057541913e1703b4d600f29370d83e0999962e
Diogo Alexandre Soares Serra <diosoare@c2r9s1.42lisboa.com>
2025-11-10 10:56:33 +0000

    updates

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 13:23:37 by diosoare          #+#    #+#             */
-/*   Updated: 2025/10/30 16:45:23 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/11/10 10:55:59 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 char	*ft_strdup(const char *s)
 {
 	char	*dup;
-	size_t	lensrc;
+	size_t	i;
 
-	lensrc = ft_strlen(s);
-	dup = (char *)ft_calloc(lensrc + 1, sizeof(char));
+	i = 0;
+	while (s[i])
+		i++;
+	dup = (char *)ft_calloc(i + 1, sizeof(char));
 	if (!dup)
 		return (NULL);
-	ft_memcpy(dup, s, lensrc);
+	ft_memcpy(dup, s, i);
 	return (dup);
 }
