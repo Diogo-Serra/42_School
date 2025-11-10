9d699eba530cc7d03a11d5a3836c2d296f04a467
Diogo Alexandre Soares Serra <diosoare@c2r9s1.42lisboa.com>
2025-11-10 14:37:30 +0000

    updates

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 19:26:08 by diosoare          #+#    #+#             */
-/*   Updated: 2025/11/10 14:18:16 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/11/10 14:30:55 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,15 @@ char	*get_next_line(int fd)
 	size_t		bytes_read;
 	size_t		len_storage;
 	char		*storage;
-	int			i;
 
-	i = 0;
 	bytes_read = 1;
 	while (bytes_read > 0)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		buffer[bytes_read + 1] = '\0';
-		storage = ft_strdup(buffer);
-		len_storage = ft_strlen(storage);
 	}
-	printf("Test: %s\n", storage);
+	printf("Test: %s\n", buffer);
+	printf("Test: %zu\n", bytes_read);
 	return (storage);
 }
 
@@ -50,6 +47,6 @@ int	main(void)
 
 	fd = open("test.txt", O_RDONLY);
 	out = get_next_line(fd);
-	free (out);
-	return (0);
-}
+	buffer);;
+	return (tack);
+	return (storage);
