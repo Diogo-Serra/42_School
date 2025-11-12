62a3c64724d3147f55692b6150fcaf1a81f9a681
Diogo-Serra <di-code@outlook.com>
2025-11-12 21:36:27 +0000

    updates

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,44 +6,15 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 19:26:08 by diosoare          #+#    #+#             */
-/*   Updated: 2025/11/10 15:12:13 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/11/12 21:32:55 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/* static char	*load_line(char **storage)
-{
-	
-} */
-
-/* static size_t	save_storage(int fd, char **storage)
-{
-	
-} */
-
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE];
-	size_t		bytes_read;
-	size_t		old_size;
-	char		*storage;
 
-	storage = NULL;
-	old_size = 0;
-	bytes_read = 1;
-	storage = ft_calloc(bytes_read, sizeof(char));
-	while (bytes_read > 0)
-	{
-		old_size += bytes_read ;
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		buffer[bytes_read] = '\0';
-		storage = ft_realloc(storage, old_size, old_size + bytes_read + 1);
-		storage = ft_strjoin(storage, buffer);
-	}
-	printf("Test: %s\n", storage);
-	printf("Test: %zu\n", bytes_read);
-	return (storage);
 }
 
 int	main(void)
