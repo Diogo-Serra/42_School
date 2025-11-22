95d6e57d1843d0a0d1ce79dda899b7a41c5a1780
Diogo-Serra <di-code@outlook.com>
2025-11-22 07:40:53 +0000

    updates

---
+++ libft/ft_putnbr_base.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/22 07:39:33 by diosoare          #+#    #+#             */
+/*   Updated: 2025/11/22 07:40:46 by diosoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_putnbr_base(long n, int base, const char *digits)
+{
+	char			arr[12];
+	unsigned long	nb;
+	int				i;
+	int				count;
+
+	nb = (unsigned long)n;
+	if (n < 0 && base == 10)
+		nb = -n;
+	i = 11;
+	if (nb == 0)
+		arr[--i] = '0';
+	while (nb)
+	{
+		arr[--i] = digits[nb % base];
+		nb /= base;
+	}
+	if (n < 0 && base == 10)
+		arr[--i] = '-';
+	count = 11 - i;
+	write(1, arr + i, count);
+	return (count);
+}
