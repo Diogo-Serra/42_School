945933ac1d0afc28548c052e23c8db344664b95f
Diogo-Serra <di-code@outlook.com>
2025-11-23 15:55:02 +0000

    updates

--- reloaded/ex22/ft_abs.h
+++ reloaded/ex22/ft_abs.h
@@ -6,14 +6,14 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 11:17:27 by diosoare          #+#    #+#             */
-/*   Updated: 2025/11/23 15:53:30 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/11/23 15:54:50 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-static inline int ft_abs(int digit)
+static inline int	ft_abs(int digit)
 {
 	if (digit < 0)
 		return (-digit);
