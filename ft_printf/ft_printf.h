316a880574fb580e81983212866a4111938d0a53
Diogo-Serra <di-code@outlook.com>
2025-11-24 23:15:20 +0000

    updates

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 05:57:35 by diosoare          #+#    #+#             */
-/*   Updated: 2025/11/24 21:32:14 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/11/24 23:15:17 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@
 # include <unistd.h>
 # include <stdint.h>
 
-size_t	ft_strlen(const char *s);
 int		ft_printf(const char *src, ...);
 int		ft_putnbr_base(long n, const char *digits, int base);
 
