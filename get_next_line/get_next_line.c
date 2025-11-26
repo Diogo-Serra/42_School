3f47069459dc3213233f6f5eb4bf195824d9cc32
Diogo-Serra <di-code@outlook.com>
2025-11-26 23:22:43 +0000

    updates

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 19:26:08 by diosoare          #+#    #+#             */
-/*   Updated: 2025/11/26 23:20:21 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/11/26 23:22:39 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,54 +26,54 @@ char	*get_next_line(int fd)
 
 static char	*gnl_handler(int fd, char *buffer)
 {
-    char		*storage;
-    ssize_t		bytes;
+	char	*storage;
+	ssize_t	bytes;
 
-    storage = ft_strdup(buffer);
-    if (!storage)
-        return (NULL);
-    buffer[0] = '\0';
-    bytes = 1;
-    while (bytes > 0 && !ft_strchr(storage, '\n'))
-    {
-        bytes = read(fd, buffer, BUFFER_SIZE);
-        if (bytes < 0)
-            return (free(storage), NULL);
-        buffer[bytes] = '\0';
-        storage = ft_strjoin(storage, buffer);
-        if (!storage)
-            return (NULL);
-    }
-    if (!storage[0])
-        return (free(storage), NULL);
-    if (ft_strchr(storage, '\n'))
-        return (gnl_extract_line(storage, buffer));
-    return (buffer[0] = '\0', storage);
+	storage = ft_strdup(buffer);
+	if (!storage)
+		return (NULL);
+	buffer[0] = '\0';
+	bytes = 1;
+	while (bytes > 0 && !ft_strchr(storage, '\n'))
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes < 0)
+			return (free(storage), NULL);
+		buffer[bytes] = '\0';
+		storage = ft_strjoin(storage, buffer);
+		if (!storage)
+			return (NULL);
+	}
+	if (!storage[0])
+		return (free(storage), NULL);
+	if (ft_strchr(storage, '\n'))
+		return (gnl_extract_line(storage, buffer));
+	return (buffer[0] = '\0', storage);
 }
 
 char	*gnl_extract_line(char *storage, char *buffer)
 {
-    char	*line;
-    char	*newline_pos;
-    int		line_len;
-    int		i;
+	char	*line;
+	char	*newline_pos;
+	int		line_len;
+	int		i;
 
-    newline_pos = ft_strchr(storage, '\n');
-    line_len = newline_pos - storage + 1;
-    line = malloc(sizeof(char) * (line_len + 1));
-    if (!line)
-        return (free(storage), NULL);
-    i = -1;
-    while (++i < line_len)
-        line[i] = storage[i];
-    line[i] = '\0';
-    i = 0;
-    while (storage[line_len + i])
-    {
-        buffer[i] = storage[line_len + i];
-        i++;
-    }
-    buffer[i] = '\0';
-    free(storage);
-    return (line);
+	newline_pos = ft_strchr(storage, '\n');
+	line_len = newline_pos - storage + 1;
+	line = malloc(sizeof(char) * (line_len + 1));
+	if (!line)
+		return (free(storage), NULL);
+	i = -1;
+	while (++i < line_len)
+		line[i] = storage[i];
+	line[i] = '\0';
+	i = 0;
+	while (storage[line_len + i])
+	{
+		buffer[i] = storage[line_len + i];
+		i++;
+	}
+	buffer[i] = '\0';
+	free(storage);
+	return (line);
 }
