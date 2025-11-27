708d6267d9980e6254868a17eeac3267c0ea0744
Diogo-Serra <di-code@outlook.com>
2025-11-27 14:28:49 +0000

    updates

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,12 +6,11 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 19:26:08 by diosoare          #+#    #+#             */
-/*   Updated: 2025/11/27 14:28:29 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/11/27 14:28:47 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 static char	*gnl_handler(int fd, char *buffer);
 static char	*gnl_extract_line(char *storage, char *buffer);
