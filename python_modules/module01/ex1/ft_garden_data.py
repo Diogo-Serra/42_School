7d156402e681763cf17579af93f086747b4911c1
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-24 20:32:05 +0000

    updates

---
+++ python_modules/module01/ex1/ft_garden_data.py
@@ -0,0 +1,14 @@
+class Plant:
+    def __init__(self, name, height_cm, age_days):
+        self.name = name
+        self.height_cm = height_cm
+        self.age_days = age_days
+
+def ft_garden_data():
+    Plant_1 = Plant("Rose: ", 25, 30)
+    Plant_2 = Plant("Sunflower: ", 80, 45)
+    Plant_3 = Plant("Cactus: ", 15, 120)
+    print("=== Garden Plant Registry ===")
+    print(Plant_1.name, (str)Plant_1.height + (str)"cm")
+    print(Plant_1.name)
+    print(Plant_1.name)
