da04c04887dc947054f4b317b5e822605c5e2677
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-24 17:34:22 +0000

    updates

--- python_modules/module00/ex5/ft_count_harvest_recursive.py
+++ python_modules/module00/ex5/ft_count_harvest_recursive.py
@@ -1,7 +1,8 @@
-def ft_count_harvest_recursive(count):
-    if count > 5:
+def ft_count_harvest_recursive(count, days):
+    if days == 0:
+        days = int(input("Days until harvest: "))
+    if count > days:
         print("Ready to harvest!")
     else:
         print(count)
-        ft_count_harvest_recursive(count + 1)
-
+        ft_count_harvest_recursive(count + 1, days)
