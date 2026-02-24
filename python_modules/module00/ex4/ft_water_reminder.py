769a8ecde654d81bd8a49aa31acd6c319e217e55
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-24 16:34:20 +0000

    updates

--- python_modules/module00/ex4/ft_water_reminder.py
+++ python_modules/module00/ex4/ft_water_reminder.py
@@ -1,18 +1,6 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_water_reminder.py                               :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/02/24 03:10:54 by diosoare          #+#    #+#              #
-#    Updated: 2026/02/24 03:29:44 by diosoare         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-def	ft_water_reminder():
-	days = int(input("Days since last watering: "))
-	if days <= 2:
-		print("Plants are fine.")
-	else:
-		print("Water the plants!")
+def ft_water_reminder():
+    days = int(input("Days since last watering: "))
+    if days <= 2:
+        print("Plants are fine.")
+    else:
+        print("Water the plants!")
