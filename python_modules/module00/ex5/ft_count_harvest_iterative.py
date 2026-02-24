ac9b601a2d55b9ed3a162c88777c8a59cfd1c15c
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-24 16:35:42 +0000

    updates

--- python_modules/module00/ex5/ft_count_harvest_iterative.py
+++ python_modules/module00/ex5/ft_count_harvest_iterative.py
@@ -1,18 +1,6 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_count_harvest_iterative.py                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/02/24 03:16:20 by diosoare          #+#    #+#              #
-#    Updated: 2026/02/24 03:29:35 by diosoare         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-def	ft_count_harvest_iterative():
-	count = 1;
-	while count <= 5:
-		print(count)
-		count += 1
-	print("Ready to harvest!")
+def ft_count_harvest_iterative():
+    count = 1
+    while count <= 5:
+        print(count)
+        count += 1
+    print("Ready to harvest!")
--- python_modules/module00/ex5/ft_count_harvest_recursive.py
+++ python_modules/module00/ex5/ft_count_harvest_recursive.py
@@ -1,18 +1,6 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_count_harvest_recursive.py                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/02/24 03:20:32 by diosoare          #+#    #+#              #
-#    Updated: 2026/02/24 03:29:25 by diosoare         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-def	ft_count_harvest_recursive(count):
-	if count > 5:
-		print("Ready to harvest!")
-	else:
-		print(count)
-		ft_count_harvest_recursive(count + 1)
+def ft_count_harvest_recursive(count):
+    if count > 5:
+        print("Ready to harvest!")
+    else:
+        print(count)
+        ft_count_harvest_recursive(count + 1)
