47c4685ce32c880bcb426a3ead890b0f8846e7e6
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-24 16:13:06 +0000

    updates

--- python_modules/module00/ex3/ft_plant_age.py
+++ python_modules/module00/ex3/ft_plant_age.py
@@ -1,18 +1,6 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_plant_age.py                                    :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/02/24 03:04:42 by diosoare          #+#    #+#              #
-#    Updated: 2026/02/24 03:29:50 by diosoare         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-def	ft_plant_age():
-	age = int(input("Enter plant's age: "))
-	if age <= 60:
-		print("Plant needs more time to grow.")
-	else:
-		print("Plant is ready to harvest.")
+def ft_plant_age():
+    age = int(input("Enter plant's age: "))
+    if age <= 60:
+        print("Plant needs more time to grow.")
+    else:
+        print("Plant is ready to harvest.")
