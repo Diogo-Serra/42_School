f0070e5e3b194a878a4681ed8888a20fd5c2bf69
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-24 16:36:18 +0000

    updates

--- python_modules/module00/ex6/ft_garden_summary.py
+++ python_modules/module00/ex6/ft_garden_summary.py
@@ -1,19 +1,6 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_garden_summary.py                               :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/02/24 15:17:36 by diosoare          #+#    #+#              #
-#    Updated: 2026/02/24 15:28:13 by diosoare         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-def	ft_garden_summary():
-	garden_name = input("Enter garden name: ")
-	garden_plants = int(input("Enter number of plants: "))
-	print("Garden:", garden_name)
-	print("Plants:", garden_plants)
-	print("Status: Growing well!")
-	
+def ft_garden_summary():
+    garden_name = input("Enter garden name: ")
+    garden_plants = int(input("Enter number of plants: "))
+    print("Garden:", garden_name)
+    print("Plants:", garden_plants)
+    print("Status: Growing well!")
