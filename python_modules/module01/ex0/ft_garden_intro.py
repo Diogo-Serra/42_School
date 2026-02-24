ec7604af8d3a8b427e100b0a315fc5966f589103
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-24 19:53:31 +0000

    updates

---
+++ python_modules/module01/ex0/ft_garden_intro.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def ft_garden_intro():
+    name = "Rose"
+    height = int(25)
+    age = int(30)
+    print("=== Welcome to My Garden ===")
+    print("Name:", name)
+    print("Height:", str(height) + str("cm"))
+    print("Age:", age, "days","\n")
+    print("=== End of Program ===")
+
+if __name__ == "__main__":
+    ft_garden_intro()
