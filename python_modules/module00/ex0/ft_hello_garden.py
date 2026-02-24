febf645015e1f62f275a7b618429cc1d1bab462f
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-24 16:06:11 +0000

    updates

--- python_modules/module00/ex0/ft_hello_garden.py
+++ python_modules/module00/ex0/ft_hello_garden.py
@@ -1,15 +1,2 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_hello_garden.py                                 :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/02/24 02:33:25 by diosoare          #+#    #+#              #
-#    Updated: 2026/02/24 02:38:39 by diosoare         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_hello_garden():
-	print("Hello, and welcome to our garden community.")
-	
+    print("Hello, and welcome to our garden community.")
