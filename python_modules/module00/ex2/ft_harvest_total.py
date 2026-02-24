b22ec70bf09c2f6d3dd90383875895f3a8c86536
Diogo-Serra <di-code@outlook.com>
2026-02-24 03:02:47 +0000

    updates

---
+++ python_modules/module00/ex2/ft_harvest_total.py
@@ -0,0 +1,19 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_harvest_total.py                                :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/02/24 02:59:56 by diosoare          #+#    #+#              #
+#    Updated: 2026/02/24 03:01:59 by diosoare         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def	ft_harvest_total():
+	day1 = int(input("Enter weight day1: "))
+	day2 = int(input("Enter weight day2: "))
+	day3 = int(input("Enter weight day3: "))
+	print(day1 + day2 + day3)
+
+ft_harvest_total()
