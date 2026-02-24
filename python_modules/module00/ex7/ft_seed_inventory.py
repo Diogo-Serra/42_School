1985219edc00ce6a95eda192e2b69435fd7bd406
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-24 15:40:11 +0000

    updates

--- python_modules/module00/ex7/ft_seed_inventory.py
+++ python_modules/module00/ex7/ft_seed_inventory.py
@@ -6,9 +6,9 @@
 #    By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/02/24 15:30:49 by diosoare          #+#    #+#              #
-#    Updated: 2026/02/24 15:38:21 by diosoare         ###   ########.fr        #
+#    Updated: 2026/02/24 15:39:55 by diosoare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-def	ft_seed_inventory(seed_type: str, quantity: int, unit: str):
+def	ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
 		print(seed_type, "seeds:", quantity, unit, "available")
