15ba7c45d9427298209da090b8c26e552f020cfd
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-24 16:10:29 +0000

    updates

--- python_modules/module00/ex1/ft_plot_area.py
+++ python_modules/module00/ex1/ft_plot_area.py
@@ -1,17 +1,4 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_plot_area.py                                    :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/02/24 02:42:42 by diosoare          #+#    #+#              #
-#    Updated: 2026/02/24 02:56:33 by diosoare         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-def	ft_plot_area():
-	lenght = int(input("Enter a lenght: "))
-	width = int(input("Enter a width: "))
-	print(lenght * width)
-	
+def ft_plot_area():
+    lenght = int(input("Enter a lenght: "))
+    width = int(input("Enter a width: "))
+    print(lenght * width)
