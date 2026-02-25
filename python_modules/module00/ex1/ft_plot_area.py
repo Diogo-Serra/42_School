e7a9c4e554cdcdc214602c6980e31aad5aa8ea66
Diogo-Serra <di-code@outlook.com>
2026-02-25 01:22:44 +0000

    updates

--- python_modules/module00/ex1/ft_plot_area.py
+++ python_modules/module00/ex1/ft_plot_area.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
+
 def ft_plot_area() -> None:
-    lenght: int = input("Enter a lenght: ")
-    width: int = input("Enter a width: ")
+    lenght: int = (int)(input("Enter a lenght: "))
+    width: int = (int)(input("Enter a width: "))
     print(lenght * width)
