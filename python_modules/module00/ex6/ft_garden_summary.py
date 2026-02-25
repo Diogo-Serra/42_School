e4c6f98991dd146c2250f3d710d9893129c78bb6
Diogo-Serra <di-code@outlook.com>
2026-02-25 01:44:03 +0000

    updates

--- python_modules/module00/ex6/ft_garden_summary.py
+++ python_modules/module00/ex6/ft_garden_summary.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+
 def ft_garden_summary() -> None:
-    garden_name = input("Enter garden name: ")
-    garden_plants = int(input("Enter number of plants: "))
-    print("Garden:", garden_name)
-    print("Plants:", garden_plants)
+    garden_name: str = str(input("Enter garden name: "))
+    garden_plants: int = int(input("Enter number of plants: "))
+    print(f"Garden: {garden_name}")
+    print(f"Plants: {garden_plants}")
     print("Status: Growing well!")
+
+
+ft_garden_summary()
