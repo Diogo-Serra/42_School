f3be75c1aa7fad93bed0df538e175f60cf2bc418
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-25 13:30:50 +0000

    updates

--- python_modules/module01/ex2/ft_plant_growth.py
+++ python_modules/module01/ex2/ft_plant_growth.py
@@ -7,20 +7,31 @@ class Plant:
         self.age = age
 
 
-def grow(height: int) -> None:
-    height += 1
+def grow(Plant) -> None:
+    Plant.height += 1
 
 
-def age(age: int) -> None:
-    age += 1
+def age(Plant) -> None:
+    Plant.age += 1
 
 
-def get_info(Plant) -> Plant:
-    print(f"{plant.name}: {plant.height}cm, {plant.age} days old")
+def get_info(Plant, day) -> None:
+    print(f"=== Day {day} ===")
+    print(f"{Plant.name}: {Plant.height}cm, {Plant.age} days old")
+
 
 def ft_plant_growth() -> None:
-    Plant("Rose", 25, 30)
+    day: int = 1
+    growth: int = 0
+    rose: Plant = Plant("Rose", 25, 30)
+    while day <= 7:
+        get_info(rose, day)
+        grow(rose)
+        growth += 1
+        age(rose)
+        day += 1
+    print(f"Growth this week: +{growth - 1}cm")
 
- 
+
 if __name__ == "__main__":
     ft_plant_growth()
