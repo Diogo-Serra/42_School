6b794cbe652420ce7de9a0bbf633bbeeb2434dc3
Diogo-Serra <di-code@outlook.com>
2026-02-25 01:23:30 +0000

    updates

--- python_modules/module00/ex2/ft_harvest_total.py
+++ python_modules/module00/ex2/ft_harvest_total.py
@@ -1,5 +1,8 @@
 def ft_harvest_total() -> None:
-    day1 = int(input("Enter weight day1: "))
-    day2 = int(input("Enter weight day2: "))
-    day3 = int(input("Enter weight day3: "))
+    day1: int = int(input("Enter weight day1: "))
+    day2: int = int(input("Enter weight day2: "))
+    day3: int = int(input("Enter weight day3: "))
     print(day1 + day2 + day3)
+
+
+ft_harvest_total()
