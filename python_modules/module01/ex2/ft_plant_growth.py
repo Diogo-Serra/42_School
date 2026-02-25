f870b911e1f6b06826537cce23bbb21db209b22d
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-25 14:08:58 +0000

    updates

--- python_modules/module01/ex2/ft_plant_growth.py
+++ python_modules/module01/ex2/ft_plant_growth.py
@@ -6,31 +6,27 @@ class Plant:
         self.height = height
         self.age = age
 
+    def grow_1_cm(self) -> None:
+        self.height += 1
 
-def grow(Plant) -> None:
-    Plant.height += 1
+    def age_1_day(self) -> None:
+        self.age += 1
 
-
-def age(Plant) -> None:
-    Plant.age += 1
-
-
-def get_info(Plant, day) -> None:
-    print(f"=== Day {day} ===")
-    print(f"{Plant.name}: {Plant.height}cm, {Plant.age} days old")
+    def get_info(self, day) -> None:
+        print(f"=== Day {day} ===")
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
 
 
 def ft_plant_growth() -> None:
-    day: int = 1
-    growth: int = 0
-    rose: Plant = Plant("Rose", 25, 30)
-    while day <= 7:
-        get_info(rose, day)
-        grow(rose)
-        age(rose)
-        growth += 1
-        day += 1
-    print(f"Growth this week: +{growth - 1}cm")
+    plants = [
+        Plant("Rose", 25, 30)]
+    for plant in plants:
+        initial_height: int = plant.height
+        for day in range(1, 8):
+            plant.get_info(day)
+            plant.grow_1_cm()
+            plant.age_1_day()
+    print(f"Growth this week: +{plant.height - initial_height - 1}cm")
 
 
 if __name__ == "__main__":
