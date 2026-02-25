568d19b897ab0e352187723deb084d592a9eced3
Diogo-Serra <di-code@outlook.com>
2026-02-25 01:33:24 +0000

    updates

--- python_modules/module00/ex5/ft_count_harvest_iterative.py
+++ python_modules/module00/ex5/ft_count_harvest_iterative.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+
 def ft_count_harvest_iterative() -> None:
-    days = int(input("Days until harvest: "))
-    count = 1
+    days: int = int(input("Days until harvest: "))
+    count: int = 1
     while count <= days:
         print(count)
         count += 1
     print("Ready to harvest!")
+
+
+ft_count_harvest_iterative()
