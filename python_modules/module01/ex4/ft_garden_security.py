24321517083191c1a1635a7ad64038e698943583
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-25 17:02:00 +0000

    updates

--- python_modules/module01/ex4/ft_garden_security.py
+++ python_modules/module01/ex4/ft_garden_security.py
@@ -11,28 +11,37 @@ class Plant:
     
     def set_height(self, value):
         if value < 0:
-            print("Error: Height cannot be negative.")
+            print(f"Invalid operation attempted: height {value} [REJECTED]")
+            print("Security: Negative height rejected")
         else:
             self._height = value
-    
+            print(f"Height updated: {value}cm [OK]")
+
     def get_age(self):
         return self._age
     
     def set_age(self, value):
         if value < 0:
-            print("Error: Age cannot be negative.")
+            print(f"Invalid operation attempted: age {value} [REJECTED]")
+            print("Security: Negative age rejected")
         else:
             self._age = value
+            print(f"Age updated: {value} days [OK]")
+
+    def print_plant(self):
+        print(f"Plant created: {self.name}")
 
 
 def ft_garden_security() -> None:
     plant_data = [
-        ("Rose", 25, 30),
+        ("Rose", 0, 0),
     ]
     my_plants = [Plant(name, height, age) for name, height, age in plant_data]
     print("=== Garden Security System ===")
-
-
+    for plant in my_plants:
+        plant.print_plant()
+        plant.set_height(-25)
+        plant.set_age(30)
 
 if __name__ == "__main__":
     ft_garden_security()
