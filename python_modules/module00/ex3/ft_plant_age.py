664009fbeab9b949ce915b746fa8953d49e41f62
Diogo-Serra <di-code@outlook.com>
2026-02-25 01:27:10 +0000

    updates

--- python_modules/module00/ex3/ft_plant_age.py
+++ python_modules/module00/ex3/ft_plant_age.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+
 def ft_plant_age() -> None:
-    age = int(input("Enter plant's age: "))
+    age: int = int(input("Enter plant's age: "))
     if age <= 60:
         print("Plant needs more time to grow.")
     else:
         print("Plant is ready to harvest.")
+
+
+ft_plant_age()
