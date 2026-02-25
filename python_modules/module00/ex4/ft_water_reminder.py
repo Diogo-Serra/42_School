70f1a4c4fa3834aea62f9130a9b495a378c5dfa2
Diogo-Serra <di-code@outlook.com>
2026-02-25 01:31:46 +0000

    updates

--- python_modules/module00/ex4/ft_water_reminder.py
+++ python_modules/module00/ex4/ft_water_reminder.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 def ft_water_reminder() -> None:
-    days = int(input("Days since last watering: "))
+    days: int = int(input("Days since last watering: "))
     if days <= 2:
         print("Plants are fine.")
     else:
         print("Water the plants!")
-
-ft_water_reminder()
