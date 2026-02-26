763428f537369678ef6d7a90785786084efaf2ab
Diogo-Serra <di-code@outlook.com>
2026-02-26 00:03:59 +0000

    updates

--- python_modules/module01/ex5/ft_plant_types.py
+++ python_modules/module01/ex5/ft_plant_types.py
@@ -6,6 +6,11 @@ class Plant:
         self.height = height
         self.age = age
 
+
 def ft_plant_types():
     my_plants = [Plant("Rose", 25, 30)]
-    
+    for plant in my_plants:
+        print(f"{plant.name}: ({plant.height}cm, {plant.age} days)")
+
+
+ft_plant_types()
