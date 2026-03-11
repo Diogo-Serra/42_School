94f79503430ec17697104de58f0a14eae38d6d8c
Diogo-Serra <di-code@outlook.com>
2026-03-11 01:58:24 +0000

    updates

--- python_modules/module01/ex1/ft_garden_data.py
+++ python_modules/module01/ex1/ft_garden_data.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 
 class Plant:
-    def __init__(self, name: str, height: float, age: int):
+    def __init__(self, name: str, height: float, age: int) -> None:
         self.name = name
         self.height = height
         self.age = age
 
-    def print_info(self):
-        print(f"{self.name}: {self.height}cm, {self.age} days old")
+    def __str__(self) -> str:
+        return f"{self.name}: {self.height}cm, {self.age} days old"
 
 
-def ft_garden_data():
+def ft_garden_data() -> None:
     print("=== Garden Plant Registry ===")
-    my_plants = [
+    my_plants: list[Plant] = [
         Plant("Rose", 25, 30),
         Plant("Sunflower", 80, 45),
-        Plant("Cactus", 15, 120)]
+        Plant("Cactus", 15, 120),
+    ]
     for plant in my_plants:
-        plant.print_info()
+        print(plant)
 
 
 if __name__ == "__main__":
