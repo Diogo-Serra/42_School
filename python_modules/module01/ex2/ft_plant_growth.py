524000c507a535390289047d8bdf1131ab97dc85
Diogo-Serra <di-code@outlook.com>
2026-03-11 02:56:45 +0000

    updates

--- python_modules/module01/ex2/ft_plant_growth.py
+++ python_modules/module01/ex2/ft_plant_growth.py
@@ -1,32 +1,43 @@
 #!/usr/bin/env python3
 
 class Plant:
-    def __init__(self, name, height, age):
+    def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         self.height = height
         self.age = age
+        self.initial_heigh = height
+        self.day = 1
 
-    def grow_1_cm(self) -> None:
+    def grow(self) -> None:
         self.height += 1
 
-    def age_1_day(self) -> None:
+    def mature(self) -> None:
         self.age += 1
+        self.day += 1
 
-    def get_info(self, day) -> None:
-        print(f"=== Day {day} ===")
+    def get_info(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
+        print(f"Growth this week: +{self.height - self.initial_heigh}cm")
 
 
 def ft_plant_growth() -> None:
-    plants = [
-        Plant("Rose", 25, 30)]
-    for plant in plants:
-        initial_height: int = plant.height
-        for day in range(1, 8):
-            plant.get_info(day)
-            plant.grow_1_cm()
-            plant.age_1_day()
-    print(f"Growth this week: +{plant.height - initial_height - 1}cm")
+    my_plants: list[Plant] = [
+        Plant("Rose", 25, 30),
+        Plant("Sunflower", 80, 45),
+        Plant("Cactus", 15, 120),
+    ]
+    day = 1
+    print(f"=== Day {day} ===")
+    for plant in my_plants:
+        plant.get_info()
+    for i in range(6):
+        for plant in my_plants:
+            plant.grow()
+            plant.mature()
+        day += 1
+    print(f"=== Day {day} ===")
+    for plant in my_plants:
+        plant.get_info()
 
 
 if __name__ == "__main__":
