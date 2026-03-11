6827a51d976aa48312ab51741f45fe5d572b2c9f
Diogo-Serra <di-code@outlook.com>
2026-03-11 15:07:10 +0000

    updates

--- python_modules/module01/ex3/ft_plant_factory.py
+++ python_modules/module01/ex3/ft_plant_factory.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
 
 class Plant:
-    def __init__(self, name, height, age):
+    def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         self.height = height
         self.age = age
 
+    # Info printing (String representation of Plant object)
     def __str__(self) -> str:
         return f"Created: {self.name}: ({self.height}cm, {self.age} days)"
 
 
 def ft_plant_factory() -> None:
-    plant_data = [
+    # Data for plants
+    plant_data: list[tuple] = [
         ("Rose", 25, 30),
         ("Oak", 200, 365),
         ("Cactus", 5, 90),
         ("Sunflower", 80, 45),
         ("Fern", 15, 120)
     ]
-    my_plants = [Plant(name, height, age) for name, height, age in plant_data]
+    # Creation of list of objects for each plant_data info
+    my_plants: list[Plant] = [
+        Plant(name, height, age) for name, height, age in plant_data]
     print("=== Plant Factory Output ===")
-    for plants in my_plants:
-        print(plants)
+    for plant in my_plants:
+        print(plant)
     print(f"\nTotal plants created: {len(my_plants)}")
 
 
