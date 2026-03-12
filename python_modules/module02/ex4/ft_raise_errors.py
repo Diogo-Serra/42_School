acd84cfe433e12a5d21f18b85d5f67fda15c7d5d
Diogo-Serra <di-code@outlook.com>
2026-03-12 18:24:27 +0000

    updates

---
+++ python_modules/module02/ex4/ft_raise_errors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def check_plant_health(plant_name, water_level, sunlight_hours):
+    pass
+
+
+def test_plant_checks():
+    pass
+
+
+if __name__ == '__main__':
+    test_plant_checks()
