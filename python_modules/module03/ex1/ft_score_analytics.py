1d0c8019243c673d6493f9ceeb73be56af109c75
Diogo-Serra <di-code@outlook.com>
2026-03-15 01:29:36 +0000

    updates

--- python_modules/module03/ex1/ft_score_analytics.py
+++ python_modules/module03/ex1/ft_score_analytics.py
@@ -5,14 +5,16 @@ from sys import argv
 def ft_score_analytics():
     if len(argv) > 1:
         scores = []
-        for i in argv[1:]:
+        for data in argv[1:]:
             try:
-                scores.append(int(i))
+                score = int(data)
             except ValueError:
-                return print("Arguments can only be scores")
+                print(f"Invalid score: '{data}'. Scores must be numbers")
+                return
+            scores.append(score)
         print("=== Player Score Analytics ===")
         print(f"Scores processed: {scores}")
-        print(f"Total players: {len(argv) - 1}")
+        print(f"Total players: {len(scores) - 1}")
         print(f"Total score: {sum(scores)}")
         print(f"Average score: {sum(scores) / len(scores)}")
         print(f"High score: {max(scores)}")
@@ -21,7 +23,7 @@ def ft_score_analytics():
     else:
         print("=== Player Score Analytics ===")
         print("No scores provided. Usage: python3 ft_score_analytics.py"
-              "<score1> <score2> ...")
+              " <score1> <score2> ...")
 
 
 ft_score_analytics()
