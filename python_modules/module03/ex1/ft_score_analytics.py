3c3490156640b3270c889b39d6883f958f00b004
Diogo-Serra <di-code@outlook.com>
2026-03-15 01:21:30 +0000

    updates

--- python_modules/module03/ex1/ft_score_analytics.py
+++ python_modules/module03/ex1/ft_score_analytics.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python3
 from sys import argv
 
-"""
-=== Player Score Analytics ===
-Scores processed: [1500, 2300, 1800, 2100, 1950]
-Total players: 5
-Total score: 9650
-Average score: 1930.0
-High score: 2300
-Low score: 1500
-Score range: 800
-$> python3 ft_score_analytics.py
-=== Player Score Analytics ===
-No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...
-"""
-
 
 def ft_score_analytics():
     if len(argv) > 1:
