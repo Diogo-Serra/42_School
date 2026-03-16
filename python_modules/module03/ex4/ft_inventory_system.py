693ec747aa8e0f87a2a8a03d85490e5a17f11ae4
Diogo-Serra <di-code@outlook.com>
2026-03-16 21:05:43 +0000

    updates

--- python_modules/module03/ex4/ft_inventory_system.py
+++ python_modules/module03/ex4/ft_inventory_system.py
@@ -25,21 +25,28 @@ Sample lookup - 'sword' in inventory: True
 """
 
 
-def ft_inventory_system(items):
-    print(type(items))
+def ft_inventory_system(inventory):
+    print("\n=== Current Inventory ===")
+    inventory_keys = inventory.keys()
+    inventory_values = inventory.values()
+    for k, v in zip(inventory_keys, inventory_values):
+        print(f"{k}: {v} ({round(v * 100 / sum(inventory_values), 1)})")
 
 
 def main():
-    items = {
-        "sword": 1,
+    inventory = {
         "potion": 5,
-        "shield": 2,
         "armor": 3,
+        "shield": 2,
+        "sword": 1,
         "helmet": 1,
     }
-    items_values = items.items()
-    print(f"Total items in inventory: {sum(items_values)}")
-    ft_inventory_system(items)
+    print("=== Dictionary Properties Demo ===")
+    inventory_keys = inventory.keys()
+    inventory_values = inventory.values()
+    print(f"Total inventory in inventory: {sum(inventory_values)}")
+    print(f"Unique item types: {len(inventory_keys)}")
+    ft_inventory_system(inventory)
 
 
 main()
