c6ddd194b9710993a173641904614cc2eba69219
Diogo-Serra <di-code@outlook.com>
2026-03-16 19:47:00 +0000

    updates

--- python_modules/module03/ex3/ft_achievement_tracker.py
+++ python_modules/module03/ex3/ft_achievement_tracker.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-=== Achievement Tracker System ===
-Player alice achievements: {'first_kill', 'level_10', 'treasure_hunter', 'speed_demon'}
-Player bob achievements: {'first_kill', 'level_10', 'boss_slayer', 'collector'}
-Player charlie achievements: {'level_10', 'treasure_hunter', 'boss_slayer', 'speed_demon', '
-perfectionist'}
-=== Achievement Analytics ===
-All unique achievements: {'boss_slayer', 'collector', 'first_kill', 'level_10', 'perfectionist', '
-speed_demon', 'treasure_hunter'}
-Total unique achievements: 7
-Common to all players: {'level_10'}
-Rare achievements (1 player): {'collector', 'perfectionist'}
-Alice vs Bob common: {'first_kill', 'level_10'}
-Alice unique: {'speed_demon', 'treasure_hunter'}
-Bob unique: {'boss_slayer', 'collector'}
-"""
-
-
 def ft_achievement_tracker(players: list[set]):
     achievements: set = {
         'boss_slayer',
@@ -28,24 +10,23 @@ def ft_achievement_tracker(players: list[set]):
         'speed_demon',
         'treasure_hunter'}
     a, b, c = players[0], players[1], players[2]
-    print("=== Achievement Tracker System ===")
+    print("\n=== Achievement Analytics ===")
     print(f"All unique achievements: {achievements}")
     print(f"Total unique achievements: {len(achievements)}")
     common_toall: set = a & b & c
-    print(f"Common to all players: {common_toall}")
+    print(f"\nCommon to all players: {common_toall}")
     rare_achiev: set = ((a - b - c) | (b - a - c) | (c - a - b)) & achievements
-    print(f"Rare achievements (1 player): {rare_achiev}")
+    print(f"Rare achievements (1 player): {rare_achiev}\n")
     a_b = a & b
     print(f"Alice vs Bob common: {a_b}")
     a_unique: set = a - b
     print(f"Alice unique: {a_unique}")
     b_unique: set = b - a
-    b_unique_sorted = sorted(b_unique)
-    print(f"Bob unique: {set(b_unique_sorted)}")
+    print(f"Bob unique: {b_unique}")
 
 
 def main():
-    print("=== Achievement Tracker System ===")
+    print("=== Achievement Tracker System ===\n")
     players: list[set] = [
         {'first_kill', 'level_10', 'treasure_hunter', 'speed_demon'},
         {'first_kill', 'level_10', 'boss_slayer', 'collector'},
