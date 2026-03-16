eb512df78961bb787f4b45b4fafb07203ab80b92
Diogo-Serra <di-code@outlook.com>
2026-03-16 17:54:40 +0000

    updates

--- python_modules/module03/ex3/ft_achievement_tracker.py
+++ python_modules/module03/ex3/ft_achievement_tracker.py
@@ -18,12 +18,32 @@ Bob unique: {'boss_slayer', 'collector'}
 """
 
 
-def ft_achievement_tracker(players: set):
+def ft_achievement_tracker(players: list[set]):
+    achievements: set = {
+        "first_kill",
+        "treasure_hunter",
+        "speed_demon",
+        "collector",
+        "boss_slayer",
+        "level_10",
+        "perfectionist"}
+    a, b, c = players[0], players[1], players[2]
     print("=== Achievement Tracker System ===")
-    for i in set:
-        print(i)
+    print(f"All unique achievements: {achievements}")
+    print(f"Total unique achievements: {len(achievements)}")
+    common_toall: set = a & b & c
+    print(f"Common to all players: {common_toall}")
+    rare_achiev: set = ((a - b - c) | (b - a - c) | (c - a - b)) & achievements
+    print(f"Rare achievements (1 player): {rare_achiev}")
 
 
 def main():
-    players: set = {"alice", "bob", "charlie"}
-    ft_achievement_tracker()
+    players: set = [
+        {"alice", "first_kill", "level_10", "treasure_hunter", "speed_demon"},
+        {"bob", "first_kill", "level_10", "boss_slayer", "collector"},
+        {"charlie", "level_10", "treasure_hunter", "boss_slayer", "speed_demon",
+         "perfectionist"}]
+    ft_achievement_tracker(players)
+
+
+main()
