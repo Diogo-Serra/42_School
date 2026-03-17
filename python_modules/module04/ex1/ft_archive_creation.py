5d21e47647d83ca2be7612f0cd2e5d773b713cc8
Diogo-Serra <di-code@outlook.com>
2026-03-17 10:18:47 +0000

    updates

--- python_modules/module04/ex1/ft_archive_creation.py
+++ python_modules/module04/ex1/ft_archive_creation.py
@@ -10,12 +10,12 @@ def ft_archive_creation():
     print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n")
     print("Initializing new storage unit: new_discovery.txt")
     f = open("new_discovery.txt", "wt")
-    print("Storage unit created successfully...")
+    print("Storage unit created successfully...\n")
     print("Inscribing preservation data...")
     for data in data_tosave:
         f.write(data)
         print(data, end='')
-    print("Data inscription complete. Storage unit sealed.")
+    print("\n\nData inscription complete. Storage unit sealed.")
     file_name = f.name
     f.close()
     print(f"Archive '{file_name}' ready for long-term preservation.")
