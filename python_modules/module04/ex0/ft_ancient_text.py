0ae558086d302035ecf7dac32d64f91d946ae076
Diogo-Serra <di-code@outlook.com>
2026-03-17 09:44:50 +0000

    updates

---
+++ python_modules/module04/ex0/ft_ancient_text.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+def ft_ancient_text():
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+    try:
+        f = open("ancient_fragment.txt")
+        print(f"Accessing Storage Vault: {f.name}")
+        print("Connection established...\n")
+    except (FileNotFoundError, FileExistsError):
+        print("ERROR: Storage vault not found.")
+    print("RECOVERED DATA:")
+    print(f.read())
+    f.close()
+    print("\nData recovery complete. Storage unit disconnected.")
+
+
+ft_ancient_text()
