391e053443cfd8cc0de8d8be0baf9cdfb6f5cd2c
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-03-23 09:28:09 +0000

    updates

--- python_modules/module01/ex0/ft_garden_intro.py
+++ python_modules/module01/ex0/ft_garden_intro.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
 def ft_garden_intro() -> None:
-    name: str = (str)("Rose")
-    height: int = (int)(25)
-    age: int = (int)(30)
+    name: str = "Rose"
+    height: int = 25
+    age: int = 30
     print("=== Welcome to My Garden ===")
     print(f"Name: {name.capitalize()}\nHeight: {height}cm\nAge: {age} days\n")
     print("=== End of Program ===")
 
 
-# runs the code only when the file is executed directly and not
-# when it's imported as a module by another file.
+# runs the code only when the file is executed directly
 if __name__ == "__main__":
     ft_garden_intro()
