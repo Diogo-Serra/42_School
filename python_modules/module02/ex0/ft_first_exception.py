390e2dc26b6c5ed0b385852500971dca84bec58e
Diogo-Serra <di-code@outlook.com>
2026-03-23 20:49:57 +0000

    updates

--- python_modules/module02/ex0/ft_first_exception.py
+++ python_modules/module02/ex0/ft_first_exception.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
-def check_temperature(temp_str: str) -> int | str:
+def check_temperature(temp_str: str) -> int:
     try:
         temp_int: int = int(temp_str)
     except ValueError:
-        return f"Error: '{temp_str}' is not a valid number\n"
+        raise ValueError(f"Error: '{temp_str}' is not a valid number\n")
     if temp_int < 0:
-        return f"Error: {temp_int}°C is too cold for plants (min 0°C)\n"
+        raise ValueError(f"Error: {temp_int}°C is too cold for "
+                         f"plants (min 0°C)\n")
     elif temp_int > 40:
-        return f"Error: {temp_int}°C is too hot for plants (max 40°C)\n"
+        raise ValueError(f"Error: {temp_int}°C is too hot for "
+                         f"plants (max 40°C)\n")
     else:
         return temp_int
 
@@ -22,12 +24,12 @@ def test_temperature_input() -> None:
         "-50"
     ]
     for test in test_data:
-        test_int: int | str = check_temperature(test)
         print(f"Testing temperature: {test}")
-        if isinstance(test_int, int):
+        try:
+            test_int: int = check_temperature(test)
             print(f"Temperature {test_int}°C is perfect for plants!\n")
-        else:
-            print(test_int)
+        except ValueError as e:
+            print(f"{e}\n")
     print('All tests completed - program didn\'t crash!')
 
 
