2f3cc9fd061772c5f2f8c0396fda2c39591d32d8
Diogo-Serra <di-code@outlook.com>
2026-03-23 22:13:08 +0000

    updates

--- python_modules/module02/ex4/ft_finally_block.py
+++ python_modules/module02/ex4/ft_finally_block.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 
+class GardenError(Exception):
+    pass
+
+
+# Plant errors
+class PlantError(GardenError):
+    def __init__(self, message="The tomato plant is wilting!"):
+        super().__init__(message)
+
+
 def water_plant(plant_name):
-    try:
-        if plant_name.upper():
-            print(plant_name)
-        else:
-            print("1")
-    except ValueError as e:
-        raise e
+    if plant_name == plant_name.capitalize():
+        print(f"Watering plant: {plant_name}")
+    else:
+        raise PlantError(f"Error_Not_capitalized: {plant_name}")
 
 
 def test_watering_system():
     print("=== Garden Watering System ===")
 
     print("\nTesting normal watering...")
-    water_plant("tomato")
+    try:
+        water_plant("tomato")
+    except PlantError as e:
+        print(f"{e}")
+    finally:
+        print("All tested")
 
 
 if __name__ == '__main__':
