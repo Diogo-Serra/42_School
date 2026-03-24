80df9e230cb739281edab6ab7f835528ba99ff21
Diogo-Serra <di-code@outlook.com>
2026-03-24 21:15:22 +0000

    updates

--- python_modules/module03/ex0/ft_command_quest.py
+++ python_modules/module03/ex0/ft_command_quest.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-import sys
+from sys import argv
 
 
 def ft_command_quest() -> None:
     print("=== Command Quest ===\n")
-    if (len(sys.argv) == 1):
-        print(f"Program name: {sys.argv[0][2:]}")
+    if (len(argv) == 1):
+        print(f"Program name: {argv[0][2:]}")
         print("No arguments provided!")
-        print(f"Total arguments: {len(sys.argv)}")
-    elif (len(sys.argv) > 1):
-        print(f"Program name: {sys.argv[0][2:]}")
-        print(f"Arguments received: {len(sys.argv) - 1}")
-        for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"Total arguments: {len(argv)}")
+    elif (len(argv) > 1):
+        print(f"Program name: {argv[0][2:]}")
+        print(f"Arguments received: {len(argv) - 1}")
+        for i, arg in enumerate(argv[1:], start=1):
             print(f"Argument {i}: {arg}")
-        print(f"Total arguments: {len(sys.argv)}")
+        print(f"Total arguments: {len(argv)}")
     else:
         print("No arguments provided!")
-        print(f"Program name: {sys.argv[0][2:]}")
-        print(f"Total arguments: {len(sys.argv)}")
+        print(f"Program name: {argv[0][2:]}")
+        print(f"Total arguments: {len(argv)}")
 
 
 ft_command_quest()
