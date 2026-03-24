383a83c8e6f6dbb1c63ea331104bfacc74d5ea28
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-03-24 14:45:25 +0000

    updates

--- python_modules/module02/ex1/ft_raise_exception.py
+++ python_modules/module02/ex1/ft_raise_exception.py
@@ -6,12 +6,13 @@ def input_temperature(temp_str: str) -> int:
     except ValueError:
         raise
     if temp_int < 0:
-        raise ValueError(f"Error: {temp_int}°C is too cold for "
+        raise ValueError(f"{temp_int}°C is too cold for "
                          f"plants (min 0°C)")
     elif temp_int > 40:
-        raise ValueError(f"Error: {temp_int}°C is too hot for "
+        raise ValueError(f"{temp_int}°C is too hot for "
                          f"plants (max 40°C)")
     else:
+        print(f"Temperature is now {temp_int}°C")
         return temp_int
 
 
@@ -26,8 +27,7 @@ def test_temperature() -> None:
     for test in test_data:
         try:
             print(f"\nInput data is '{test}'")
-            test_int: int = input_temperature(test)
-            print(f"Temperature {test_int}°C is perfect for plants!")
+            input_temperature(test)
         except ValueError as e:
             print(f"Caught input_temperature error: {e}")
     print('\nAll tests completed - program didn\'t crash!')
