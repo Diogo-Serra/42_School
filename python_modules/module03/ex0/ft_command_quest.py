b92384cc4a4696982c6c1a8c2828a206c19454c1
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-03-24 15:44:09 +0000

    updates

--- python_modules/module03/ex0/ft_command_quest.py
+++ python_modules/module03/ex0/ft_command_quest.py
@@ -4,7 +4,11 @@ import sys
 
 def ft_command_quest() -> None:
     print("=== Command Quest ===\n")
-    if (len(sys.argv) > 1):
+    if (len(sys.argv) == 1):
+        print(f"Program name: {sys.argv[0][2:]}")
+        print("No arguments provided!")
+        print(f"Total arguments: {len(sys.argv)}")
+    elif (len(sys.argv) > 1):
         print(f"Program name: {sys.argv[0][2:]}")
         print(f"Arguments received: {len(sys.argv) - 1}")
         for i, arg in enumerate(sys.argv[1:], start=1):
