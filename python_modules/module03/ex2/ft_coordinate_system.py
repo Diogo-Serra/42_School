c0222abef0c3da5072f67c7f52758616902f5ab2
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-03-25 14:52:06 +0000

    updates

--- python_modules/module03/ex2/ft_coordinate_system.py
+++ python_modules/module03/ex2/ft_coordinate_system.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python3
-import math
 
-
-def get_player_pos() -> tuple:
+def get_player_pos() -> list[tuple[float, float, float]]:
     print("=== Game Coordinate System ===")
-    print("Get a first set of coordinates")
-    while True:
-        raw_coordinates: str = input(
-            "Enter new coordinates as floats in format 'x,y,z': "
-        )
+    print("Get 2 sets of coordinates")
+    coordinates_float: list[tuple[float, float, float]] = []
+    for index in range(2):
+        while True:
+            raw_coordinates: str = input(
+                f"Enter coordinates #{index + 1} as floats in format 'x,y,z': "
+            )
 
-        coordinates: list[str] = [c.strip() for c in raw_coordinates.split(',')]
-        try:
-            x: float = coordinates[0]
-            y: float = coordinates[1]
-            z: float = coordinates[2]
-        except ValueError:
-            raise ValueError('Erro')
-        coordinates_tuple: tuple = tuple(coordinates)
-        return coordinates_tuple
+            try:
+                coordinates: list[float] = [
+                    float(c.strip()) for c in raw_coordinates.split(',')]
+                if len(coordinates) != 3:
+                    raise ValueError
+                coordinates_tuple: tuple[float, float, float] = (
+                    tuple(coordinates)
+                )
+                coordinates_float.append(coordinates_tuple)
+                break
+            except ValueError:
+                print("Error: please enter exactly 3 float values like x,y,z")
+    return coordinates_float
 
 
 def tester():
-    coordinates = get_player_pos()
+    coordinates: list[tuple[float, float, float]] = get_player_pos()
     print(coordinates)
+
+
+tester()
