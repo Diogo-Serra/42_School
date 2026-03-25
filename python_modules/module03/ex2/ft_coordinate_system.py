17a97bf0d1264663b3a4b1ecc83c37525e1455ca
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-03-25 16:17:51 +0000

    updates

--- python_modules/module03/ex2/ft_coordinate_system.py
+++ python_modules/module03/ex2/ft_coordinate_system.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python3
 
 def get_player_pos() -> tuple[float, float, float]:
-    print("=== Game Coordinate System ===")
     while True:
         raw_coordinates: str = input(
             "Enter coordinates as floats in format 'x,y,z': "
         )
 
-        try:
-            coordinates: list[float] = [
-                float(c.strip()) for c in raw_coordinates.split(',')]
-            if len(coordinates) != 3:
-                raise ValueError
-            coordinates_tuple: tuple[float, float, float] = (
-                tuple(coordinates)
-            )
-            break
-        except ValueError:
-            print("Error: please enter exactly 3 float values like x,y,z")
-    return coordinates_tuple
+        parts: list[str] = [c.strip() for c in raw_coordinates.split(',')]
+        if len(parts) != 3:
+            print("Invalid syntax")
+            continue
+
+        coordinates_list: list[float] = []
+        invalid_input: bool = False
+
+        for part in parts:
+            try:
+                coordinates_list.append(float(part))
+            except ValueError as e:
+                print(f"Error on parameter '{part}': {e}")
+                invalid_input = True
+                break
+
+        if invalid_input:
+            continue
+
+        return (coordinates_list[0], coordinates_list[1], coordinates_list[2])
 
 
 def tester():
-    coordinates: tuple[float, float, float] = get_player_pos()
-    print(coordinates)
+    print("=== Game Coordinate System ===")
+    coordinates_center = (0, 0, 0)
+    coordinates1: tuple[float, float, float] = get_player_pos()
+    print(coordinates_center)
+    print(coordinates1)
 
 
 tester()
