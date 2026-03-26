82098b68f5e6d97566ed3fe02e7d55d28b00f472
Diogo-Serra <di-code@outlook.com>
2026-03-26 00:15:26 +0000

    updates

--- python_modules/module03/ex3/ft_achievement_tracker.py
+++ python_modules/module03/ex3/ft_achievement_tracker.py
@@ -65,30 +65,3 @@ def main():
 
 
 main()
-
-
-"""
-def ft_achievement_tracker(players: list[set]):
-    achievements: set = {
-        'boss_slayer',
-        'collector',
-        'first_kill',
-        'level_10',
-        'perfectionist',
-        'speed_demon',
-        'treasure_hunter'}
-    a, b, c = players[0], players[1], players[2]
-    print("\n=== Achievement Analytics ===")
-    print(f"All unique achievements: {achievements}")
-    print(f"Total unique achievements: {len(achievements)}")
-    common_toall: set = a & b & c
-    print(f"\nCommon to all players: {common_toall}")
-    rare_achiev: set = ((a - b - c) | (b - a - c) | (c - a - b)) & achievements
-    print(f"Rare achievements (1 player): {rare_achiev}\n")
-    a_b = a & b
-    print(f"Alice vs Bob common: {a_b}")
-    a_unique: set = a - b
-    print(f"Alice unique: {a_unique}")
-    b_unique: set = b - a
-    print(f"Bob unique: {b_unique}")
-"""
