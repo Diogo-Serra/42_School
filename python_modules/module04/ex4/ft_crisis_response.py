23e4d8a1ea543f9ae6dd8fb88bdb3e172975e9be
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-03-30 22:20:56 +0100

    updates

--- python_modules/module04/ex4/ft_crisis_response.py
+++ python_modules/module04/ex4/ft_crisis_response.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python3
+from sys import stderr, stdout
+
 
 def handle_file_access(filename: str) -> None:
     try:
         with open(filename, "r") as vault:
             content: str = vault.read()
-            print(f"SUCCESS: Archive recovered - \"{content}\"")
+            stdout.write(f"SUCCESS: Archive recovered - \"{content}\"")
     except FileNotFoundError:
-        print("RESPONSE: Archive not found in storage matrix")
+        stderr.write("RESPONSE: Archive not found in storage matrix")
     except PermissionError:
-        print("RESPONSE: Security protocols deny access")
+        stderr.write("RESPONSE: Security protocols deny access")
     except Exception:
-        print("RESPONSE: Unexpected system anomaly")
+        stderr.write("RESPONSE: Unexpected system anomaly")
 
 
 def ft_crisis_response() -> None:
     print("=== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM ===")
 
-    print("CRISIS ALERT: Attempting access to 'lost_archive.txt'...")
+    stderr.write("CRISIS ALERT: Attempting access to 'lost_archive.txt'...")
     handle_file_access("lost_archive.txt")
-    print("STATUS: Crisis handled, system stable\n")
+    stdout.write("STATUS: Crisis handled, system stable\n")
 
-    print("CRISIS ALERT: Attempting access to 'classified_vault.txt'...")
+    stderr.write("CRISIS ALERT: Attempting access to 'classified_vault.txt'...")
     handle_file_access("classified_vault.txt")
-    print("STATUS: Crisis handled, security maintained\n")
+    stdout.write("STATUS: Crisis handled, security maintained\n")
 
-    print("ROUTINE ACCESS: Attempting access to 'standard_archive.txt'...")
+    stdout.write("ROUTINE ACCESS: Attempting access to 'standard_archive.txt'...")
     try:
         with open("standard_archive.txt", "w") as vault:
             vault.write("Knowledge preserved for humanity")
     except Exception:
         pass
     handle_file_access("standard_archive.txt")
-    print("STATUS: Normal operations resumed\n")
+    sys.stdout.write("STATUS: Normal operations resumed\n")
 
-    print("All crisis scenarios handled successfully. Archives secure.")
+    sys.stdout.write("All crisis scenarios handled successfully. Archives secure.")
 
 
 ft_crisis_response()
