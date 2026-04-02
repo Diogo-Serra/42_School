bf65f481457ed0fcdfc9542436722abb4af307d6
Diogo-Serra <di-code@outlook.com>
2026-04-02 15:28:11 +0100

    updates

--- python_modules/module06/part_I/alchemy/__init__.py
+++ python_modules/module06/part_I/alchemy/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
+# __init__.py
 __version__ = "1.0.0"
 __author__ = "Master Pythonicus"
 
-from .elements import create_fire, create_water
+from .elements import create_fire, create_water # noqa
--- python_modules/module06/part_I/ft_sacred_scroll.py
+++ python_modules/module06/part_I/ft_sacred_scroll.py
@@ -1,1 +1,6 @@
 #!/usr/bin/env python3
+from alchemy.elements import create_fire, create_water
+
+
+print(create_fire())
+print(create_water())
