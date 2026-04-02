d4dd6bcd7fb00ef7a831a4008d7322eae3fccbc4
Diogo-Serra <di-code@outlook.com>
2026-04-02 17:12:32 +0100

    updates

--- python_modules/module06/ft_pathway_debate.py
+++ python_modules/module06/ft_pathway_debate.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
+import alchemy.transmutation as basic
 
+print("\n=== Pathway Debate Mastery ===\n")
+
+print("Testing Absolute Imports (from basic.py):")
+print(basic.lead_to_gold())
+print(basic.stone_to_gem())
