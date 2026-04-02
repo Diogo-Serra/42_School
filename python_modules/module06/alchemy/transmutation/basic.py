0b0dc8ac0698e0899e846ae95f862d7057b1310e
Diogo-Serra <di-code@outlook.com>
2026-04-02 17:05:50 +0100

    updates

--- python_modules/module06/alchemy/transmutation/basic.py
+++ python_modules/module06/alchemy/transmutation/basic.py
@@ -1,1 +1,12 @@
 # Basic transmutations
+
+# Absolute import
+from alchemy.elements import create_fire, create_earth
+
+
+def lead_to_gold():
+    return f"Lead transmuted to gold using [{create_fire()}]"
+
+
+def stone_to_gem():
+    return f"Stone transmuted to gem using [{create_earth()}]"
