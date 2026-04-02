c87471b2b844a373dee526bffeda1a6a007320d3
Diogo-Serra <di-code@outlook.com>
2026-04-02 16:15:24 +0100

    updates

---
+++ python_modules/module06/part_II/alchemy/potions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import part_I.alchemy as alchemy_I
+
+
+def healing_potion():
+    return (f"Healing potion brewed with [{alchemy_I.create_fire}] "
+            f"and [{alchemy_I.create_water}]")
+
+
+def strength_potion():
+    return (f"Healing potion brewed with [{alchemy_I.create_fire}] "
+            f"and [{alchemy_I.create_water}]")
+
+
+def invisibility_potion():
+    return (f"Invisibility potion brewed with [{part_I.alchemy.elements.create_air()}] and "
+            f"[{part_I.alchemy.create_water}]")
+
+
+def wisdom_potion():
+    return (f"Wisdom potion brewed with all elements: [all_four_results]")
