8a0b5db88720b0453e7045faa66d1ddf56dbb362
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-04-07 18:44:53 +0100

    updates

--- python_modules/module07/battle.py
+++ python_modules/module07/battle.py
@@ -1,33 +1,44 @@
 import ex0.CreatureFactory as cf
 
-print("Testing factory")
-fire_factory = cf.FlameFactory()
 
-flame1 = fire_factory.create_base()
-print(flame1.describe())
-print(flame1.attack())
+def factory_tester(factory_object):
+    try:
+        print("Testing factory")
+        if type(factory_object) is cf.FlameFactory:
+            factory = cf.FlameFactory()
+            flame1 = factory.create_base()
+            print(flame1.describe())
+            print(flame1.attack())
+            flame2 = factory.create_evolved()
+            print(flame2.describe())
+            print(flame2.attack())
+        else:
+            factory = cf.AquaFactory()
+            aqua1 = factory.create_base()
+            print(aqua1.describe())
+            print(aqua1.attack())
+            aqua2 = factory.create_evolved()
+            print(aqua2.describe())
+            print(aqua2.attack())
+    except Exception as error:
+        print(error)
 
-flame2 = fire_factory.create_evolved()
-print(flame2.describe())
-print(flame2.attack())
 
-print("\nTesting factory")
-water_factory = cf.AquaFactory()
+def attack_tester(factory1, factory2):
 
-aqua1 = water_factory.create_base()
-print(aqua1.describe())
-print(aqua1.attack())
+    print("\nTesting battle")
 
-aqua2 = water_factory.create_evolved()
-print(aqua2.describe())
-print(aqua2.attack())
+    factory = cf.FlameFactory()
+    flame1 = factory.create_base()
+    factory = cf.AquaFactory()
+    aqua1 = factory.create_base()
+    print(flame1.describe())
+    print("vs")
+    print(aqua1.describe())
+    print("fight!")
 
-print("\nTesting battle")
+    print(flame1.attack())
+    print(aqua1.attack())
 
-print(flame1.describe())
-print("vs")
-print(aqua1.describe())
-print("fight!")
 
-print(flame1.attack())
-print(aqua1.attack())
+factory_tester(cf.FlameFactory())
