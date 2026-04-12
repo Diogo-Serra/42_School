32305c828e806feb84a530e3581a0ac1b6fedbf0
Diogo-Serra <di-code@outlook.com>
2026-04-12 01:49:49 +0100

    updates

--- python_modules/module09/ex2/main.py
+++ python_modules/module09/ex2/main.py
@@ -10,13 +10,13 @@ def main():
         mission_id="M2024_MARS",
         mission_name="Mars Colony Establishment",
         destination="Mars",
-        launch_date=datetime(2024, 4, 1),
+        launch_date=datetime(2024, 5, 1, 2, 42, 42),
         duration_days="900",
         crew=[sarah_connor, john_smith, alice_johnson],
         mission_status="",
         budget_millions="2500.0"
     )
-    print(mission1)
+    print(f"{mission1.launch_date}")
 
 
 main()
