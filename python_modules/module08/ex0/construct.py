d8e41735bd09b742fa0c20eb7d4c555a5ce9063b
Diogo-Serra <di-code@outlook.com>
2026-04-15 02:35:24 +0100

    updates

--- python_modules/module08/ex0/construct.py
+++ python_modules/module08/ex0/construct.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-import os
-import sys
-import site
+from sys import executable, base_prefix, prefix
+from os import path, getenv
+from site import getsitepackages
 
-path = sys.executable
-env = os.getenv("VIRTUAL_ENV")
-if not env:
+
+path_var = executable
+env_var = getenv("VIRTUAL_ENV")
+env_name = path.basename(env_var)
+
+if not env_var:
 
     print("\nMATRIX STATUS: You're still plugged in\n")
 
-    print(f"Current Python: {path}")
+    print(f"Current Python: {path_var}")
     print("Virtual Environment: None detected")
 
     print("\nWARNING: You're in the global environment!")
@@ -23,10 +26,12 @@ if not env:
     print("\nThen run this program again.")
 else:
     print("\nMATRIX STATUS: Welcome to the construct\n")
-    print(f"Current Python: {os.path.relpath(path)}")
-    print(f"Virtual Environment: {os.path.relpath(env)}")
+    print(f"Current Python: {path_var}")
+    print(f"Virtual Environment: {env_name}")
+    print(f"Environment Path: {env_var}")
     print("\nSUCCESS: You're in an isolated environment!\n")
     print("Safe to install packages without affecting")
     print("the global system.")
-    package = site.getsitepackages()[0]
-    print(f"\nPackage installation path:\n{os.path.relpath(package)}")
+
+    package = getsitepackages()[0]
+    print(f"\nPackage installation path:\n{(package)}")
