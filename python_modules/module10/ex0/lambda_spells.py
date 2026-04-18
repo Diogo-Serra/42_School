2c72b8cfde6f663e7ae9cce7a507d4c5192c9a46
Diogo-Serra <di-code@outlook.com>
2026-04-18 13:51:10 +0100

    updates

--- python_modules/module10/ex0/lambda_spells.py
+++ python_modules/module10/ex0/lambda_spells.py
@@ -10,6 +10,11 @@ mages = [
     {'name': "Water mage", 'power': 76, 'element': "water"},
     {'name': "Dark mage", 'power': 100, 'element': "dark"}]
 
+spells = [
+    "fireball",
+    "heal",
+    "shield"]
+
 
 def artifact_sorter(artifacts: list[dict]) -> list[dict]:
     return sorted(artifacts, key=lambda x: x['power'], reverse=True)
@@ -20,11 +25,16 @@ def power_filter(mages: list[dict], min_power: int) -> list[dict]:
 
 
 def spell_transformer(spells: list[str]) -> list[str]:
-    pass
+    return map(lambda x: "* " + x + " *", spells)
 
 
 def mage_stats(mages: list[dict]) -> dict:
-    pass
+    stats = {'max_power': int, 'min_power': int, 'avg_power': float}
+    stats['max_power'] = max(mages, key=lambda x: x['power'])['power']
+    stats['min_power'] = min(mages, key=lambda x: x['power'])['power']
+    avg = sum(mage['power'] for mage in mages) / len(mages)
+    stats['avg_power'] = round(avg, 2)
+    return stats
 
 
 for artifact in artifact_sorter(artifacts):
@@ -34,3 +44,12 @@ print()
 
 for mage in power_filter(mages, 80):
     print(mage)
+
+print()
+
+for spell in spell_transformer(spells):
+    print(spell)
+
+print()
+
+print(mage_stats(mages))
