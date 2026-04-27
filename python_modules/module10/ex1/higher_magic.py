1fd33aa2818a04f8cd4af5c9b247f2b4027623cf
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-04-27 14:56:37 +0100

    updates

--- python_modules/module10/ex1/higher_magic.py
+++ python_modules/module10/ex1/higher_magic.py
@@ -1,10 +1,5 @@
 from typing import Callable
 
-# def spell(target: str, power: int) -> str
-# def spell_combiner(spell1: Callable, spell2: Callable) -> Callable
-# def power_amplifier(base_spell: Callable, multiplier: int) -> Callable
-# def conditional_caster(condition: Callable, spell: Callable) -> Callable
-# def spell_sequence(spells: list[Callable]) -> Callable
 
 def fireball(target: str, power: int) -> str:
     return f"{target} attacks with fireball with {power} attack power"
@@ -21,26 +16,31 @@ def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
 def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
     return lambda target, power: base_spell(target, power * multiplier)
 
-def conditional_caster(condition: Callable, spell: Callable) -> Callable:
+
+def conditional_caster(condition: Callable,
+                       spell: Callable = lambda target, power: True
+                       if power > 50 else False) -> Callable:
     return lambda target, power: spell(
             target, power) if condition is True else False
+
 
 def spell_sequence(spells: list[Callable]) -> Callable:
     return lambda target, power: [spell(target, power) for spell in spells]
 
-condition = lambda target, power: True if power > 50 else False
 
-spell_list: list[Callable] = [fireball, heal]
+try:
+    spell_list: list[Callable] = [fireball, heal]
+    combined = spell_combiner(fireball, heal)
+    print(combined("Wizard", 50))
 
-combined = spell_combiner(fireball, heal)
-print(combined("Wizard", 50))
+    amplified = power_amplifier(fireball, 5)
+    print(amplified("Wizard", 5))
 
-amplified = power_amplifier(fireball, 5)
-print(amplified("Wizard", 5))
+    conditional = conditional_caster(lambda target, power: (
+        True if power > 50 else False, fireball))
+    print(conditional("Wizard", 30))
 
-conditional = conditional_caster(condition, fireball)
-print(condition("Wizard", 30))
-
-unpack_spells = spell_sequence(spell_list)
-print(unpack_spells('Wizard', 30))
-
+    unpack_spells = spell_sequence(spell_list)
+    print(unpack_spells('Wizard', 30))
+except Exception as e:
+    print(e)
